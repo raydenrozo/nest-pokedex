a08b9050ef5f8c079271195eec2e299815ba9c8a
Jonathan Rozo <jonathan.rozohernandez@cencosud.cl>
2023-02-18 17:15:28 -0300

    Semilla y Paginacion

---
+++ src/seed/seed.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { CommonModule } from 'src/common/common.module';
+import { SeedService } from './seed.service';
+import { SeedController } from './seed.controller';
+import { PokemonModule } from '../pokemon/pokemon.module';
+
+@Module({
+  controllers: [SeedController],
+  providers: [SeedService],
+  imports: [
+    PokemonModule,
+    CommonModule
+  ]
+})
+export class SeedModule {}
