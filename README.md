6c321f3f99ac9438322b188b2a4b47506c955b6b
Jonathan Rozo <jonathan.rozohernandez@cencosud.cl>
2023-02-26 23:11:26 -0300

    Listo para deployar en railway

--- README.md
+++ README.md
@@ -18,12 +18,27 @@ npm i -g @nestjs/cli
 ````
 docker-compose up -d
 ````
-5. Reconstruir la base de datos con la semilla
-````
-http://localhost:3000/api/v2/seed
+
+5. Clonar el archivo __.env.template__ y renombrar la copia a __.env__
+
+6. Llenar las variables de entorno definidas en el __.env__
+
+7. Ejecutar la aplicación en dev
+```
+yarn start:dev
 ```
 
+8. Reconstruir la base de datos con la semilla
+```
+http://localhost:3000/api/v2/seed
+```
 
 ## Stack Usuado
 * MongoDB
 * Nest
+
+## Mongo DB 
+## raydenrozo
+## passs: PVuQmUQZTh4rKz9G
+## IP Address: 186.10.141.160
+## mongodb+srv://raydenrozo:<password>@cluster0.tayts.mongodb.net/test
--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -1,4 +1,5 @@
 import { BadRequestException, Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { isValidObjectId, Model } from 'mongoose';
 import { Pokemon } from './entities/pokemon.entity';
 import { CreatePokemonDto } from './dto/create-pokemon.dto';
@@ -8,11 +9,15 @@ import { PaginationDto } from 'src/common/dto/pagination.dto';
 
 @Injectable()
 export class PokemonService {
-
+  private  defaultLimit: number;
   constructor(
     @InjectModel(Pokemon.name)
-    private readonly pokemonModel: Model<Pokemon>
-  ){}
+    private readonly pokemonModel: Model<Pokemon>,
+    private readonly configService: ConfigService
+    
+  ){
+    this.defaultLimit = this.configService.get<number>('defaultLimit');
+  }
   async create(createPokemonDto: CreatePokemonDto) {
     createPokemonDto.name = createPokemonDto.name.toLocaleLowerCase();
 
@@ -26,8 +31,7 @@ export class PokemonService {
   }
 
   findAll(paginationDto: PaginationDto) {
-
-    const { limit = 10, offset = 0 } = paginationDto;
+    const { limit = this.defaultLimit, offset = 0 } = paginationDto;
 
     return this.pokemonModel.find()
       .limit( limit )
